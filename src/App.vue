55f341661bd4f851e0e5429cef7de387f54936c5
harleyka <harleyka@post.cz>
2024-02-21 09:49:01 +0100

    basic settings

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      yep
+    </div>
+  </header>
+
+  <main>
+    mehehe
+    <Button label="test" />
+
+  </main>
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './assets/index.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.mount('#app')
